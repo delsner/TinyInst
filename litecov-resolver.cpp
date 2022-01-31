a261841c8cbf43e3af78a92850830daa2cf264f3
Daniel Elsner <dvelsner@gmail.com>
2022-01-31 23:43:03 +0100

    Add simple lcov output strategy

--- litecov-resolver.cpp
+++ litecov-resolver.cpp
@@ -37,6 +37,13 @@ struct CoverageReport {
 	std::vector<TestCoverage> covered_tests;
 	fs::path output_file;
 
+	struct CoveredSourceFileLCOV {
+		int LF = 0; // number of instrumented lines
+		int LH = 0; // number of lines with non-zero execution count
+		std::string filename;
+		std::map<int, int> line_number_hit_count; // <line_number, hits>
+	};
+
 	void SaveAsCSV() {
 		std::fstream file_stream;
 		file_stream.open(output_file, std::fstream::out);
@@ -51,12 +58,47 @@ struct CoverageReport {
 		}
 		file_stream.close();
 	}
+
+	// Simple approach that currently doesn't care about LF/LH.
+	void SaveAsLCOV() {
+		for (const auto& covered_test : covered_tests) {
+			std::fstream file_stream;
+			file_stream.open(covered_test.test_identifier + ".info", std::fstream::out);
+			file_stream << "TN:" << covered_test.test_identifier << "\n";
+			std::map<std::string, CoveredSourceFileLCOV> covered_source_files;
+			for (const auto& covered_address : covered_test.covered_addresses) {
+				if (!covered_source_files.count(covered_address.filename)) {
+					CoveredSourceFileLCOV covered_source_file;
+					covered_source_file.filename = covered_address.filename;
+					covered_source_file.line_number_hit_count[covered_address.line_number] = 1;
+					covered_source_file.LF++;
+					covered_source_file.LH++;
+					covered_source_files[covered_address.filename] = covered_source_file;
+				}
+				else {
+					covered_source_files[covered_address.filename].line_number_hit_count[covered_address.line_number] = 1;
+					covered_source_files[covered_address.filename].LF++;
+					covered_source_files[covered_address.filename].LH++;
+				}
+			}
+			for (const auto& covered_source_file : covered_source_files) {
+				file_stream << "SF:" << covered_source_file.second.filename << "\n";
+				for (const auto covered_line : covered_source_file.second.line_number_hit_count) {
+					file_stream << "DA:" << covered_line.first << "," << covered_line.second << "\n";
+				}
+				file_stream << "LF:" << covered_source_file.second.LF << "\n";
+				file_stream << "LH:" << covered_source_file.second.LH << "\n";
+				file_stream << "end_of_record\n";
+			}
+			file_stream.close();
+		}
+	}
 };
 
 // Main routine.
 int main(int argc, char** argv)
 {
-	bool drop_duplicate_lines = GetBinaryOption("-drop_duplicate_lines", argc, argv, false);
+	bool drop_duplicate_lines = GetBinaryOption("-drop_duplicate_lines", argc, argv, true);
 	std::string ext = std::string(GetOptionOrDefault("-ext", argc, argv, ".cov"));
 	std::regex regex = std::regex(GetOptionOrDefault("-regex", argc, argv, ".*"));
 	std::string root = std::string(GetOptionOrDefault("-root", argc, argv, "."));
@@ -198,7 +240,7 @@ int main(int argc, char** argv)
 	}
 	printf("DEBUG: Writing collected coverage to output file %s.\n", coverage_report.output_file.string().c_str());
 	coverage_report.SaveAsCSV();
-	// TODO: Add other export strategy here for standardized output format, e.g., to visualize line coverage.
+	coverage_report.SaveAsLCOV();
 
 	return 0;
 }
